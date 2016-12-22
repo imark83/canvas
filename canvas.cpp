c3279be8ddf1f73fdebd71c71c323aefb801154c
Marcos Rodriguez <marcos@unizar.es>
2016-12-22 19:18:00 +0100

    onReshape funciona!

--- canvas.cpp
+++ canvas.cpp
@@ -8,7 +8,7 @@
 #include <triangle.hpp>
 
 Triangle tr;
-Camera cam(-2.0, 2.0, -2.0, 2.0);
+Camera cam(2.0, 2.0);
 
 void renderFunction () {
 	glClear (GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
@@ -16,16 +16,16 @@ void renderFunction () {
 
 	tr.render (cam);
 	glFlush ();
-	tr.rotate(0.1);
+	tr.move(0.00011, 0.0);
 }
+
 
 void init () {
 	glClearColor(0.0f, 0.0f, 0.0f, 1.0f);
 
 	tr.initBuffers ();
-	// tr.setScale (0.5);
-	// tr.setPosition (0.5,0.5);
-	// tr.move(-0.5,0);
+	tr.setScale (0.5);
+	tr.move(2.0, 1.0);
 
 	ShaderInfo shaders[] = {
 		{GL_VERTEX_SHADER, "default.vs.glsl"},
@@ -34,6 +34,13 @@ void init () {
 
 	tr.setProgram (loadShaders (shaders));
 	glUseProgram (tr.getProgram ());
+}
+
+void reshapeFunction(int w, int h) {
+	std::cout << "reshape! w = " << w << std::endl;
+	cam.setWidth((2.0*w)/512.0);
+	cam.setHeight((2.0*h)/512.0);
+	glViewport(0,0,w,h);
 }
 
 
@@ -53,12 +60,14 @@ int main(int argc, char *argv[]) {
 	// Must be done after glut is initialized!
 	// glewExperimental = GL_TRUE;
 	glewInit();
+	glutIdleFunc (renderFunction);
+	glutReshapeFunc(reshapeFunction);
+	glutDisplayFunc(renderFunction);
 
 
 	init ();
 
 
-	glutDisplayFunc(renderFunction);
 	glutMainLoop();
 
 
--- include/camera.hpp
+++ include/camera.hpp
@@ -5,23 +5,34 @@
 class Camera {
 public:
 
-	  float x0;
-	  float x1;
-	  float y0;
-	  float y1;
+	float x0;
+	float x1;
+	float y0;
+	float y1;
 
-	  Camera () {x0=y0=0.0; x1=y1=1.0;}
-	  Camera (float _x0, float _x1, float _y0, float _y1) {
-		    x0 = _x0; x1 = _x1; y0 = _y0; y1 = _y1;
-	  }
+	Camera () {x0=y0=0.0; x1=y1=1.0;}
+	Camera (float w, float h) {x0=y0=0.0; x1=w; y1=h;}
+	Camera (float _x0, float _x1, float _y0, float _y1) {
+		x0 = _x0; x1 = _x1; y0 = _y0; y1 = _y1;
+	}
 
-	  float getWidth () {return x1-x0;}
-	  float getHeight () {return y1-y0;}
+	float getWidth () {return x1-x0;}
+	float getHeight () {return y1-y0;}
 
-	  Mat getModel () {
-		  return scaleMatrix (2.0/getWidth(), 2.0/getHeight()) *
-		  		transMatrix (-x0 - getWidth()/2.0, -y0 - getHeight()/2.0);
-	  }
+	void setWidth (float w) {
+		x1 = x0+w;
+		std::cout << "x = [" << x0 << " " << x1 << "]" << std::endl;
+		std::cout << "width = " << getWidth() << std::endl;
+	}
+	void setHeight (float h) {
+		y1 = y0+h;
+		std::cout << "y = [" << y0 << " " << y1 << "]" << std::endl;
+		std::cout << "height = " << getHeight() << std::endl;
+	}
+	Mat getModel () {
+		return scaleMatrix (2.0/getWidth(), 2.0/getHeight()) *
+				transMatrix (-x0 - getWidth()/2.0, -y0 - getHeight()/2.0);
+	}
 
 };
 
